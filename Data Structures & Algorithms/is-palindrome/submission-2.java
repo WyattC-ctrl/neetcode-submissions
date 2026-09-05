8593cc3d2b38aca5f9e42eb7fce91c1769c07766
Wyatt Cox <wmcox07@gmail.com>
2026-09-05 16:36:42 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        String filtered = s.replaceAll("[^a-zA-Z0-9]", "");
+
+        filtered = filtered.toLowerCase();
+        int j = filtered.length() - 1; 
+
+        for (int i = 0; i < filtered.length() / 2; i++){
+            if (!filtered.substring(i,i+1).equals(filtered.substring(j, j+1))){
+                return false; 
+            }
+            j--; 
+        }
+
+        return true; 
+
+        
+    }
+}
