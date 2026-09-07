1bec343feabae74739c6bb966b4f973c18a10f33
Wyatt Cox <wmcox07@gmail.com>
2026-09-06 23:49:23 -0400

    Add: binary-search - submission-9

---
+++ Data Structures & Algorithms/binary-search/submission-9.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        // binary serach is where you keep plitting array in half and checking that one element if it is less then or greater then another element
+
+
+        int mid = nums.length/2;
+        int lft = 0; 
+        int rgt = nums.length - 1; 
+
+
+        while (lft <= rgt){
+            mid  = lft + (rgt-lft)/2;
+            if (nums[mid] == target){
+                return mid; 
+            } else if (nums[mid] < target){ 
+                lft = mid + 1;
+            } else { 
+                rgt = mid - 1;
+            }
+        }
+
+        return -1; 
+        
+    }
+}
